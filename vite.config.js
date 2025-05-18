8da6467d50917a3d6a19e529830dd7a515ed085f
youssef <5874409598y@gmail.com>
2025-05-19 01:09:55 +0300

    last

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/YOUSSEF_PORTFOLIO/',
   plugins: [react()],
-  base: "/YOUSSEF_PORTFOLIO"
 })
